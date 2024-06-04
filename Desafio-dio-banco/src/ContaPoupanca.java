5bfc6c7bb309935c1874d0cf7b8528f90858a46f
cristiandoring <cristiandoring@gmail.com>
2024-06-04 08:21:25 -0300

    Abstração das classes

---
+++ Desafio-dio-banco/src/ContaPoupanca.java
@@ -0,0 +1,17 @@
+public class ContaPoupanca {
+    public int agencia;
+    public int numero_conta;
+    public double saldo;
+    
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+        
+    }
+}
